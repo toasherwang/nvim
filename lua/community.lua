c009e0b3bf6f3d9c75227c4a26e4d6dbe1a25f68
asherwang <code@toasherwang.com>
2025-04-06 14:40:43 +0800

    add yaml support

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.yaml" },
   -- import/override with your plugins folder
 }
